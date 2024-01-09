0660151197ff6c4002942c854381d1227e48c243
Nathan Nkweto <nathannkweto87@gmail.com>
2024-01-09 10:48:57 +0000

    compulsory tasks task 0 to task 11 python code and tests

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,4 @@ an instance of the specified class ; otherwise False.
 
 def inherits_from(obj, a_class):
     """ Return True or False"""
-    return(issubclass(type(obj), a_class) and type(obj) != a_class)
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
